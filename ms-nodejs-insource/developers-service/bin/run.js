e71c50581321626405ad9139d699bbf279214b06
jacinta254 <jecihjoy@gmail.com>
2021-07-31 23:44:08 +0300

    Ms - Unregister a service from service registry on shutdown

--- ms-nodejs-insource/developers-service/bin/run.js
+++ ms-nodejs-insource/developers-service/bin/run.js
@@ -15,10 +15,36 @@ server.listen(0);
 
 server.on('listening', () => {
   const registerService = () => axios.put(`http://localhost:3000/register/${config.name}/${config.version}/${server.address().port}`);
+  const unregisterService = () => axios.delete(`http://localhost:3000/register/${config.name}/${config.version}/${server.address().port}`);
 
   registerService();
 
   const interval = setInterval(registerService, 2000);
+  const cleanup = async () => {
+    clearInterval(interval);
+    await unregisterService();
+  };
+
+  process.on('uncaughtException', async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
+  /** CTR+C exit */
+  process.on('SIGINT', async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
+  process.on('SIGTERM', async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
+  /** Testing  uncaughtException*/
+  setTimeout(() => {
+    throw new Error('Something happened');
+  }, 10000);
 
   log.info(
     `Hi there! I'm listening on port ${server.address().port} in ${service.get('env')} mode.`,
