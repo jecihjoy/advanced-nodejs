e19e476009d8cd9f736e2dcd0b13631d41f067e6
jacinta254 <jecihjoy@gmail.com>
2021-09-12 23:09:55 +0300

    Scaling node js application; forking and clustering

---
+++ scaling-nodejs/cloning/run-forking.js
@@ -0,0 +1,10 @@
+/** FORKING PROCESSES*/
+const { fork } = require('child_process')
+
+const processes = [
+  fork('./app', ['3001']),
+  fork('./app', ['3002']),
+  fork('./app', ['3003'])
+]
+
+console.log(`forked ${processes.length} processes`)
